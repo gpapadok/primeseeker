885d4640c1f8cff0a3082e781b728a4eaf8fd9e2
gpapadok <giorgos.papadokostakis@proton.me>
2024-10-24 22:32:57 +0300

    Formatting

--- primeseeker/src/primeseeker/primes.clj
+++ primeseeker/src/primeseeker/primes.clj
@@ -11,32 +11,26 @@
 (defn- ds-execute! [& query]
   (jdbc/execute! @ds query {:builder-fn rs/as-unqualified-kebab-maps}))
 
-(defn index-db
-  [n]
+(defn index-db [n]
   (ds-execute! "select * from prime where number = ?" n))
 
-(defn get-all-numbers
-  []
+(defn get-all-numbers []
   (ds-execute! "select * from prime"))
 
-(defn- add-number!
-  [p]
+(defn- add-number! [p]
   (ds-execute! "insert into prime (number, is_prime) values (?, null)" p))
 
-(defn all-numbers
-  []
+(defn all-numbers []
   (mapv :number (ds-execute! "select number from prime")))
 
-(defn create-and-add-number!
-  []
+(defn create-and-add-number! []
   (let [numbers     (all-numbers)
         max-n       (apply max numbers)
         next-number (+ max-n 2)]
     (add-number! next-number)
     next-number))
 
-(defn first-available
-  []
+(defn first-available []
   (let [unprocessed (->> (ds-execute! "select number from prime where is_prime is null")
                          (mapv :number)
                          set)
@@ -48,17 +42,14 @@
                                             processing)]
     (and (seq available) (apply min available))))
 
-(defn get-first-available
-  []
+(defn get-first-available []
   (or (first-available) (create-and-add-number!)))
 
-(defn matching-number-proc-id
-  [number proc-id]
+(defn matching-number-proc-id [number proc-id]
   (let [db-entry (@primes-db number)]
     (and (:processing db-entry) (= proc-id (:proc-id db-entry)))))
 
-(defn allocate-number-to-worker!
-  []
+(defn allocate-number-to-worker! []
   (let [uuid        (java.util.UUID/randomUUID)
         next-number (get-first-available)]
     (swap! primes-db
@@ -69,7 +60,6 @@
     {:proc-id uuid
      :number  next-number}))
 
-(defn update-number!
-  [n is-prime]
+(defn update-number! [n is-prime]
   (ds-execute! "update prime set is_prime = ? where number = ?" is-prime n)
   (swap! primes-db #(dissoc % n)))
