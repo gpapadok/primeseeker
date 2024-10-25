3305bf63cd19a75c466304b51ab96011d923955b
gpapadok <giorgos.papadokostakis@proton.me>
2024-10-25 13:10:10 +0300

    Refactor cache with protocol

--- primeseeker/src/primeseeker/primes.clj
+++ primeseeker/src/primeseeker/primes.clj
@@ -1,25 +1,55 @@
 (ns primeseeker.primes
-  (:require [next.jdbc :as jdbc])
-  (:require [next.jdbc.result-set :as rs]))
+  (:require [next.jdbc :as jdbc]
+            [next.jdbc.result-set :as rs]
+            [taoensso.telemere :as t]))
 
+;;; DB ;;;
 (def db {:dbtype "sqlite" :dbname "primes"})
-
-(let [cache (atom {})]
-  (defn cache-insert [number uuid]
-    (swap! cache
-           #(assoc % number {:processing true
-                             :proc-id    uuid})))
-  (defn cache-delete [number]
-    (swap! cache
-           #(dissco % number)))
-  (defn cache-inspect []
-    @cache))
 
 (defn create-datasource []
   (delay (jdbc/get-datasource db)))
 
 (defn- ds-execute! [ds & query]
   (jdbc/execute! @ds query {:builder-fn rs/as-unqualified-kebab-maps}))
+;;;;;;;;;;
+
+;;; CACHE ;;;
+(defprotocol PrimesCache
+  "Cache to store numbers being processed to evaluate primality"
+  (insert! [this number uuid] "Insert number")
+  (delete! [this number] "Delete number")
+  (inspect [this] "Get all"))
+
+(defn- create-atom-cache []
+  (let [cache (atom {})]
+    (reify PrimesCache
+      (insert! [this number uuid]
+        (swap! cache
+               #(assoc % number {:processing true
+                                 :proc-id    uuid})))
+      (delete! [this number] (swap! cache #(dissoc % number)))
+      (inspect [this] @cache))))
+
+;; NOTE: Method calls on this only work with dot operator for some reason
+(def ^:dynamic *cache* (create-atom-cache))
+
+(defn- cache-insert! [number uuid]
+  (t/log! :info ["Cache - number cached (v2):" number])
+  (. *cache* insert! number uuid))
+
+(defn- cache-delete! [number]
+  (t/log! :info ["Cache - number deleted (v2):" number])
+  (. *cache* delete! number))
+
+(defn- cache-all-processing []
+  (->> (. *cache* inspect)
+       (filter (fn [[n m]] (:processing m)))
+       (map first)
+       set))
+
+(defn- cache-get [n]
+  ((. *cache* inspect) n))
+;;;;;;;;;;;;;;
 
 (defn- index-db [ds n]
   (ds-execute! ds "select * from prime where number = ?" n))
@@ -44,10 +74,7 @@
   (let [unprocessed (->> (ds-execute! ds "select number from prime where is_prime is null")
                          (mapv :number)
                          set)
-        processing  (->> @primes-db
-                         (filter (fn [[n m]] (:processing m)))
-                         (map first)
-                         set)
+        processing  (cache-all-processing)
         available   (clojure.set/difference unprocessed
                                             processing)]
     (and (seq available) (apply min available))))
@@ -56,16 +83,16 @@
   (or (first-available ds) (create-and-add-number! ds)))
 
 (defn matching-number-proc-id [number proc-id]
-  (let [db-entry (@primes-db number)]
+  (let [db-entry (cache-get number)]
     (and (:processing db-entry) (= proc-id (:proc-id db-entry)))))
 
 (defn allocate-number-to-worker! [ds]
   (let [uuid        (java.util.UUID/randomUUID)
         next-number (get-first-available ds)]
-    (cache-insert next-number uuid)
+    (cache-insert! next-number uuid)
     {:proc-id uuid
      :number  next-number}))
 
 (defn update-number! [ds n is-prime]
   (ds-execute! ds "update prime set is_prime = ? where number = ?" is-prime n)
-  (cache-delete n))
+  (cache-delete! n))
