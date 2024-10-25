397bfe352a759346b07828c17a835b74b7348170
gpapadok <giorgos.papadokostakis@proton.me>
2024-10-25 13:29:36 +0300

    Update tests

--- primeseeker/test/primeseeker/primes_test.clj
+++ primeseeker/test/primeseeker/primes_test.clj
@@ -1,10 +1,24 @@
 (ns primeseeker.primes-test
   (:require [clojure.test :refer :all]
-            [primeseeker.primes :refer :all]))
+            [next.jdbc :as jdbc]
+            [primeseeker.primes :as sut]))
 
-(deftest test-create-and-add
-  (testing "test db length"
-    (let [l-1 (count @primes-db)
-          _   (create-and-add-number!)
-          l-2 (count @primes-db)]
-      (is (= (inc l-1) l-2)))))
+(def test-db {:dbtype "sqlite" :dbname "primes-test"})
+
+(def ds (delay (jdbc/get-datasource test-db)))
+
+(use-fixtures :each
+  (fn [f]
+    (let [seed-query "insert into prime (number, is_prime) values (2, true), (3, true), (5, null)"
+          delete-query "delete from prime"]
+      (#'sut/ds-execute! ds seed-query)
+      (binding [sut/*cache* (#'sut/create-atom-cache)]
+        (f))
+      (#'sut/ds-execute! ds delete-query))))
+
+(deftest test-allocate-number-to-worker
+  (testing "Test cache length"
+    (let [l-1 (count (#'sut/cache-all-processing))
+          _   (#'sut/allocate-number-to-worker! ds)
+          l-2 (count (#'sut/cache-all-processing))]
+      (is (= (inc l-1) l-2) "Cache length did not increase"))))
