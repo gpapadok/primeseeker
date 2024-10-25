ec86366cb76f5d8042d6a55ece88ead394c1e512
gpapadok <giorgos.papadokostakis@proton.me>
2024-10-25 13:26:34 +0300

    Add test database

--- primeseeker/scripts/seed-db.clj
+++ primeseeker/scripts/seed-db.clj
@@ -2,16 +2,24 @@
   (:require [next.jdbc :as jdbc]))
 
 (def db {:dbtype "sqlite" :dbname "primes"})
+(def test-db {:dbtype "sqlite" :dbname "primes-test"})
 
 (def ds (jdbc/get-datasource db))
+(def test-ds (jdbc/get-datasource test-db))
 
-(jdbc/execute! ds ["
+(def create-table "
 create table if not exists prime (
     number int auto_increment primary key,
     is_prime boolean
-)"])
+)
+")
 
-(jdbc/execute! ds ["
+(def seed "
 insert into prime (number, is_prime)
 values (2, true), (3, true), (5, null)
-"])
+")
+
+(jdbc/execute! ds [create-table])
+(jdbc/execute! ds [seed])
+(jdbc/execute! test-ds [create-table])
+(jdbc/execute! test-ds [seed])
