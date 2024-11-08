6f40c7b45a0035a34b15df833690386537ce59d3
gpapadok <giorgos.papadokostakis@proton.me>
2024-11-09 01:57:48 +0200

    formatting

--- primeseeker/test/primeseeker/primes_test.clj
+++ primeseeker/test/primeseeker/primes_test.clj
@@ -9,7 +9,7 @@
 
 (use-fixtures :each
   (fn [f]
-    (let [seed-query "insert into prime (number, is_prime) values (2, true), (3, true), (5, null)"
+    (let [seed-query   "insert into prime (number, is_prime) values (2, true), (3, true), (5, null)"
           delete-query "delete from prime"]
       (#'sut/ds-execute! ds seed-query)
       (binding [sut/*cache* (#'sut/create-atom-cache)]
