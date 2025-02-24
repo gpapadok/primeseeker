6cdfd552a9557529c0a1e90b5751efa948708f91
gpapadok <giorgos.papadokostakis@proton.me>
2025-02-24 14:31:21 +0200

    Formatting

--- primeseeker/src/clj/primeseeker/ui/main.clj
+++ primeseeker/src/clj/primeseeker/ui/main.clj
@@ -3,20 +3,19 @@
             [hiccup2.core :as h]))
 
 (defn main [body]
-  (h/html
-   (h/raw "<!DOCTYPE html>")
-   [:html
-    [:head
-     [:meta {:charset "utf-8"}]
-     [:meta {:name :viewport :content "width=device-width, initial-scale=1"}]
-     [:title "Primeseeker"]
-     [:meta {:name    :description
-             :content "Prime number database"}]
-     [:meta {:name    :author
-             :content "Giorgos Papadokostakis"}]
-     [:script {:src "https://cdn.tailwindcss.com"}] ; Remove tailwind from cdn
-     ]
-    [:body.m-10.bg-stone-100
-     [:div.flex
-      (navbar)
-      [:div.flex body]]]]))
+  (h/html (h/raw "<!DOCTYPE html>")
+    [:html
+     [:head
+      [:meta {:charset "utf-8"}]
+      [:meta {:name :viewport :content "width=device-width, initial-scale=1"}]
+      [:title "Primeseeker"]
+      [:meta {:name    :description
+              :content "Prime number database"}]
+      [:meta {:name    :author
+              :content "Giorgos Papadokostakis"}]
+      [:script {:src "https://cdn.tailwindcss.com"}] ; TODO: Remove tailwind from cdn
+      ]
+     [:body.m-10.bg-stone-100
+      [:div.flex
+       (navbar)
+       [:div.flex body]]]]))
