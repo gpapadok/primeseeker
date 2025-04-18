d3ff7cba00e59dabf894d26dc7d0d01f71022a33
gpapadok <giorgos.papadokostakis@proton.me>
2025-04-18 10:34:10 +0300

    Refactor button

--- primeseeker/src/clj/primeseeker/ui/work.clj
+++ primeseeker/src/clj/primeseeker/ui/work.clj
@@ -2,8 +2,12 @@
   (:require [primeseeker.ui.main :refer [main]]
             [hiccup2.core :as h]))
 
+(defn work-button []
+  [:div.flex.mx-20
+   [:button.rounded-xl.border-slate-100.border-2.p-3.bg-indigo-500.m-auto
+    {:class "hover:bg-stone-100 hover:border-indigo-500"
+     :onclick "primeseeker.core.do_something()"}
+    "Work"]])
+
 (defn work []
-  (main [:div
-         [:button.p-5.bg-green-600.border-100.border-black
-          {:onclick "primeseeker.core.do_something()"}
-          "Mitsos"]]))
+  (main (work-button)))
