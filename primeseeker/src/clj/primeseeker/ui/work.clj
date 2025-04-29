3660d223c696c42defc2020fcab1d2b605756d47
Giorgos Papadokostakis <gpapadok@Mac.home>
2025-04-29 18:58:39 +0300

    Add pause button

--- primeseeker/src/clj/primeseeker/ui/work.clj
+++ primeseeker/src/clj/primeseeker/ui/work.clj
@@ -2,12 +2,24 @@
   (:require [primeseeker.ui.main :refer [main]]
             [hiccup2.core :as h]))
 
-(defn work-button []
+(defn- work-button []
   [:div.flex.mx-20
    [:button.rounded-xl.border-slate-100.border-2.p-3.bg-indigo-500.m-auto
     {:class "hover:bg-stone-100 hover:border-indigo-500"
-     :onclick "primeseeker.core.do_something()"}
+     :onclick "primeseeker.view.work()"}
     "Work"]])
 
+(defn- pause-button []
+  [:div.flex.mx-20
+   [:button.rounded-xl.border-slate-100.border-2.p-3.bg-indigo-500.m-auto
+    {:class "hover:bg-stone-100 hover:border-indigo-500"
+     :onclick "primeseeker.view.pause()"}
+    "Pause"]])
+
+(defn- component []
+  [:div
+   (work-button)
+   (pause-button)])
+
 (defn work []
-  (main (work-button)))
+  (main (component)))
