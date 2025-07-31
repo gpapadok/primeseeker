39c944dbffce092a05e5a949269e41284fa29c34
gpapadok <giorgos.papadokostakis@proton.me>
2025-07-31 21:15:33 +0300

    Remove unused namespaces

--- primeseeker/src/clj/primeseeker/view.clj
+++
@@ -1,6 +0,0 @@
-(ns primeseeker.view
-  (:require [primeseeker.ui.primes :as ui.primes]
-            [primeseeker.primes :as store]))
-
-(defn primes [{:keys [datasource] :as req}]
-  (ui.primes/primes (store/get-primes datasource)))
